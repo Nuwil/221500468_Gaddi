e29f57b277c63ecb2aaa42014d3046f94593df3b
unknown <noeljgaddi@gmail.com>
2024-10-07 00:48:20 -0700

    added more js and shit

--- script.js
+++ script.js
@@ -2,12 +2,27 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(event, tabname) {
-    for (tablink of tablinks) {
+    for (var tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
-    for (tabcontent of tabcontents) {
+
+    for (var tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
+
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
+
+    var image = document.querySelector('.column-2 img');
+    if (tabname === 'skills') {
+        image.src = 'images/skills.jpg';
+    } else if (tabname === 'experience') {
+        image.src = 'images/experience.jpg';
+    } else if (tabname === 'education') {
+        image.src = 'images/education.jpg';
+    } else if (tabname === 'hobbies') {
+        image.src = 'images/hobbies.jpg';
+    } else if (tabname === 'body-info') {
+        image.src = 'images/body-info.jpg';
+    }
 }
